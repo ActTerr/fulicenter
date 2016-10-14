9e2bb893eae754f4b7e5fed980a09858ad9b4b4c
mac-yk <164322105@qq.com>
2016-10-14 16:35:27 +0800

    增加底部菜单

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.ucai.fulicenter.R;
-import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
