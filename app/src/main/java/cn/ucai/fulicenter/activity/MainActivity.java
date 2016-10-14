693e9257f183e22975cbcc3bca6f63eef99668be
mac-yk <164322105@qq.com>
2016-10-14 17:51:45 +0800

    对菜单设置点击事件监听

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -2,8 +2,10 @@ package cn.ucai.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.i("MainActivity onCreate");
+    }
+    public void onChanged(View v){
+        switch (v.getId()){
+            case R.id.rbBoutique:
+                break;
+            case R.id.rbCart:
+                break;
+            case R.id.rbNewGood:
+                break;
+            case R.id.rbCategory:
+                break;
+            case R.id.rbPersonal:
+                break;
+        }
     }
 }
