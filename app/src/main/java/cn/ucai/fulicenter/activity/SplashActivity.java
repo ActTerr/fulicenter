c0c33d6c65491f73115da542a29eb05b94158661
mac-yk <164322105@qq.com>
2016-10-14 15:29:36 +0800

    对闪屏逻辑进行修改

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package cn.ucai.fulicenter.activity;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private long time =2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                long cost=System.currentTimeMillis()-start;
+           if(time-cost>0){
+               try {
+                   Thread.sleep(time-cost);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+
+            }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }).start();
+    }
+
+}
