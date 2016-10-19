2bb5e0a29f9040d50c7670bd2e0f1ff9af6b9160
mac-yk <164322105@qq.com>
2016-10-19 15:40:49 +0800

    add BoutiqueFragment(finish)

---
+++ app/src/main/java/cn/ucai/fulicenter/fragment/BaseFragment.java
@@ -0,0 +1,32 @@
+package cn.ucai.fulicenter.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mac-yk on 2016/10/19.
+ */
+
+public class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initView();
+        initData();
+        setListener();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    protected void setListener() {
+    }
+
+    protected void initData() {
+    }
+
+    protected void initView() {
+    }
+}
